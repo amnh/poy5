e32fd5e875cc59fdd4fe6f0a7cdc4c0d36ea026a
Andres Varon <avaron@amnh.org>
2008-02-12 19:27:22 -0500

    Fixed bug in Kolmo.Encodings.l, added decoding functions and docs.
    Bugfix:
        Kolmo.Encodings.l produced the wrong result by adding 1 _before_ calculating
        the logarithm as opposed to after. This has been corrected.
    
    Improvements:
        Added decoding functions for the e family of encodes (e, hat, e_2).
    
        Added descriptions of the functions implemented by Kolmo.Encodings.

--- src/kolmo.ml
+++ src/kolmo.ml
@@ -2,22 +2,116 @@
 * An Introduction to Kolmogorov Complexity and its Applications by Li and
 * Vitanyi *)
 
-module Encodings = struct
+module type E = sig
+    (** A bit representation *)
+    type bit = Zero | One
+    (** Encoding a natural number *)
+    type encoding = bit list 
+    (* The representation of a natural number *)
+    type natural
+
+    (** Type Conversion *)
+    val to_nat : int -> natural
+    val of_nat : natural -> int
+
+    (** The length of the encoding of a natural number *)
+    val l : natural -> natural
+
+    (** THe binary encoding of a natural number *)
+    val binary : natural -> encoding
+
+    (** Some prefix free encodings *)
+    val e : natural -> natural -> encoding
+    val e_2 : natural -> encoding
+    val hat : natural -> encoding
+
+    (** Decoding the previous prefix free codifications *)
+    val decode : encoding -> natural
+end 
+
+module Encodings : E = struct
+    type bit = Zero | One
+    type encoding = bit list
+    type natural = int
+
+    let to_nat (x : int)  =
+        if x < 0 then failwith "Illegal argument"
+        else (x : natural)
+
+    let of_nat x = x
+
     (* From page 13 l(x) *)
-    let l x = int_of_float ((log (float_of_int (1 + x))) /. (log 2.)) 
+    let l (x : natural) = 
+        1 + (truncate ((log (float_of_int x)) /. (log 2.)))
+
+    let to_bit = function
+        | 0 -> Zero
+        | 1 -> One
+        | _ -> assert false
 
     (* The binary representation of an integer *)
-    let rec binary x = 
-        if x < 0 then failwith "Illegal argument"
-        else
-            if x = 0 then ""
-            else (binary (x lsr 1)) ^ (if (x land 1) = 1 then "1" else "0")
+    let binary x = 
+        if x = 0 then []
+        else 
+            let rec aux_binary x = 
+                if x = 0 then []
+                else (to_bit  (x land 1)) :: (aux_binary (x lsr 1)) 
+            in
+            List.rev (aux_binary x)
 
     (* From page 13, E_i(x) *)
     let rec e i x =
         match i with
-        | 0 -> (String.make x '1') ^ "0"
-        | i -> (e (i - 1) (l x)) ^ (binary x)
+        | 0 -> 
+                let rec make_list len acc = 
+                    if len = 0 then acc
+                    else make_list (len - 1) (One :: acc)
+                in
+                make_list x [Zero] 
+        | i -> (e (i - 1) (l x)) @ (binary x)
 
     let e_2 = e 2
+
+    let hat = e 1
+
+    let nat_of_binary x =
+        let aux_nat_of_binary acc = function
+            | One -> ((acc lsl 1) lor 1) 
+            | Zero -> (acc lsl 1) 
+        in
+        List.fold_left aux_nat_of_binary 0 x
+
+    let decode_list_of_ones lst = 
+        let rec aux x = function 
+            | Zero :: tl -> x, tl
+            | One :: lst -> aux (x + 1) lst
+            | [] -> failwith "Illegal encoding"
+        in
+        aux 0 lst
+
+    let extract_first x lst = 
+        let rec aux h cnt lst =
+            if cnt = 0 then List.rev h, lst
+            else 
+                match lst with
+                | a :: t -> aux (a :: h) (cnt - 1) t
+                | [] -> failwith "Illegal encoding"
+        in
+        aux [] x lst
+
+    let decode bin =
+        let rec decode acc to_read left =
+            match to_read with
+            | `Zero ->
+                    let x, rest_of_list = decode_list_of_ones left in
+                    decode x (`Length x) rest_of_list
+            | `Length x ->
+                    match left with
+                    | [] -> acc
+                    | left ->
+                            let number, rest_of_list = extract_first x left in
+                            let x = (nat_of_binary number) in
+                            decode x (`Length x) rest_of_list
+        in
+        decode 0 `Zero bin
 end
