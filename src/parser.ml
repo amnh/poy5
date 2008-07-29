a8953c25e1efcfbe2c32336f25223c610b7d539f
Andres Varon <avaron@amnh.org>
2008-07-28 23:12:11 -0400

    Improved the behavior of the GenBank file format parser.
    The GenBank format parser used to take the GenBank file and reconstruct all the
    information contained in the Fasta format (as produced by genbank). This had the
    tremendous disadvantage that the terminal names where useless in a tree. To fix
    this, we now extract the organism name, and replace the spaces with underscores.
    
    Much better.

--- src/parser.ml
+++ src/parser.ml
@@ -506,16 +506,22 @@ module Genbank = struct
         try
             while true do
                 let line = input_line ch in
+                (*
                 if Str.string_match (Str.regexp 
                 " *\\(VERSION+ *\\) \\([a-zA-Z0-9.]+\\) *GI:\\([0-9]+\\)") 
                 line 0 then 
                     output_string chout 
                     (">gi|" ^ (Str.matched_group 3 line) ^ "|" ^
                     (Str.matched_group 2 line) ^ "|" ^ !definition_str)
-                else if Str.string_match (Str.regexp 
-                " *\\(DEFINITION+ *\\) \\([a-zA-Z0-9 ,]+\\)") line 0 then 
-                    definition_str :=  (Str.matched_group 2 line) ^ "\n"
-                else
+                *)
+                if Str.string_match (Str.regexp 
+                " *\\(ORGANISM+ *\\) \\([a-zA-Z0-9 ,]+\\)") line 0 then begin
+                    let str = Str.matched_group 2 line in
+                    let str = Str.global_replace (Str.regexp " ") "_" str in
+                    output_string chout ">";
+                    output_string chout str;
+                    output_string chout "\n";
+                end else
                     if Str.string_match 
                     (Str.regexp " *\\([0-9]+\\) \\([a-zA-Z ]+\\)+") line 0 then
                         let temp = Str.matched_group 2 line in
