702f7ffc3c1985e9f68ed1ae2a905c130fd5056b
Andres Varon <avaron@amnh.org>
2008-10-02 17:14:24 -0400

    Transplant fixes.

--- src/tree.ml
+++ src/tree.ml
@@ -339,7 +339,7 @@ let rec get_leaves ?(acc=[]) tree handle =
     end
 
 (** [get_all_leaves tree] returns a list of all the leaves in the tree *)
-let get_all_leaves tree handle =
+let get_all_leaves tree =
     let fn handle acc = get_leaves ~acc tree handle in
     All_sets.Integers.fold fn tree.handles []
 
--- src/tree.mli
+++ src/tree.mli
@@ -245,8 +245,6 @@ module CladeFPMap : (Map.S with type key = CladeFP.fp)
 val get_vertices_to_handle :  int -> u_tree -> int list
 val fix_handle_neighbor : int -> int -> u_tree -> u_tree
 
-val get_all_leaves : u_tree -> int list
-
 (******* Tree Fusing *)
 
 val source_to_target :
