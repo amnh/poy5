4cdb1f30100b7cb5b36179653c4666456b159920
lin hong <lhong@amnh.org>
2009-09-29 17:30:41 -0400

    add interface for new function "inv_med_albert"

--- src/utlGrappa.mli
+++ src/utlGrappa.mli
@@ -79,3 +79,9 @@ val cmp_self_breakpoint_dis : int array -> int -> int
  * where [Y] is an ordered permutation of |genome|
  * For example. [genome] = (-6, 1, 5), Y = (1, 5, 6) *)
 val cmp_self_oriented_breakpoint_dis : int array -> int -> int
+
+(*
+* given three input sequence array, returns the median by calling
+* albert-median-solver. the int here is a sign of circular/or not
+* *)
+val inv_med_albert : int array -> int array -> int array -> int -> int array
