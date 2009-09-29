fc41cb2623768e4a14eb171221b4142cd51f586e
lin hong <lhong@amnh.org>
2009-09-29 17:26:23 -0400

    add function interface for "c_inv_med_albert"

--- src/grappa/grappa.mli
+++ src/grappa/grappa.mli
@@ -12,3 +12,5 @@ val c_set : genome_arr -> int -> int -> int -> unit
 val inversions : genome -> genome -> int -> int -> (int * int) list 
 val genomes : int array array -> genome_arr
 val genes : int array array -> genome array
+val c_inv_med_albert: genome -> genome -> genome -> int -> int -> 
+    (int, Bigarray.int32_elt, Bigarray.c_layout) Bigarray.Array1.t
