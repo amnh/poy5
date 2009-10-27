ce838ce0630d1a621033a62f95d160eae11b34c6
lin hong <lhong@amnh.org>
2009-10-27 17:34:11 -0400

    add function "inv_med_siepel" to utlGrappa.ml for siepel mediansolver

--- src/utlGrappa.ml
+++ src/utlGrappa.ml
@@ -113,6 +113,51 @@ let cmp_inversion_dis (genomeX : int array) (genomeY : int array) circular  =
     let inv_dis = Grappa.c_cmp_inv_dis g0 g1 num_gen circular in   
     inv_dis 
 
+let inv_med_siepel (genomeX : int array) (genomeY : int array) (genomeZ : int array)  =
+    (*  debug message *)
+     let print_intarr arr = 
+        Printf.printf "[%!";
+        Array.iter (Printf.printf "%d,%!") arr;
+        Printf.printf "],%!";
+    in
+    Printf.printf "inv_med_siepel,input seqcodes: %!";
+    print_intarr genomeX; print_intarr genomeY; print_intarr genomeZ;
+    (* debug message *)
+    let ori_genomeX = genomeX in
+    let genomeX, genomeY, genomeZ = standardize3 genomeX genomeY genomeZ in
+    (* debug msg *) 
+     Printf.printf "after standardize: %!";
+    print_intarr genomeX; print_intarr genomeY; print_intarr genomeZ;
+    (* debug msg *)
+    let num_gen = Array.length genomeX in 
+    (* There is a MAX_STR_LEN=2048 macro in grappa, if we need to work on longer sequence, 
+    * modify the macro in structs.h to accomodate the requirement*)
+    assert (num_gen>0); assert(num_gen<=2048);
+    let genome_arr = Grappa.c_create_empty_genome_arr 3 num_gen in  
+    for index = 0 to num_gen - 1 do
+        Grappa.c_set genome_arr 0 index genomeX.(index);   
+        Grappa.c_set genome_arr 1 index genomeY.(index);   
+        Grappa.c_set genome_arr 2 index genomeZ.(index);
+    done;
+    let g0 = Grappa.c_get_one_genome genome_arr 0 in
+    let g1 = Grappa.c_get_one_genome genome_arr 1 in 
+    let g2 = Grappa.c_get_one_genome genome_arr 2 in
+    let g_med3 = Grappa.c_inv_med_siepel g0 g1 g2 num_gen in
+    let len = Bigarray.Array1.dim g_med3 in
+    let intarr = Array.init len ( 
+        fun index ->
+           Int32.to_int (g_med3.{index})
+    ) in
+    let resarr = de_standardize3 ori_genomeX intarr num_gen in
+    (* debug msg *) 
+     Printf.printf "output seqcode = %!"; print_intarr resarr;
+     print_newline();
+    (* debug msg*)
+    resarr
+
+
+
+
 let inv_med_albert (genomeX : int array) (genomeY : int array) (genomeZ : int array) circular =
     let print_intarr arr = 
         Printf.printf "[%!";
