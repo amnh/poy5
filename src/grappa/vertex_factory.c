1f73689267e40e20589fce130cad70e90fccf42a
lin hong <lhong@amnh.org>
2009-12-03 11:30:49 -0500

    get rid of "return_vertex", reuse the memory block create by "new_vf". in function "get_vertex", if reach the end of current line, initialize next line if not do so before. if reach the end of the memory block, reuse the memory from beginning.

--- src/grappa/vertex_factory.c
+++ src/grappa/vertex_factory.c
@@ -10,13 +10,17 @@
 #include "vertex_factory.h"
 #include <stdio.h>
 
-#define VFWIDTH 1024
+#define VFWIDTH 512 //1024 
+//VFWIDTH should be variable, decided by gene size
+
 void
 clean_vf ( VertexFactory * vf, int ngenes, void ( *clear_mem ) ( void * ),
            void *arg )
 {
     int i, j;
     int length1;
+    vf->offseth = 0;
+    vf->offsett = length1;
     vf->nalloc = 0;
     vf->ngenes = ngenes;
     vf->clear_mem = clear_mem;
@@ -57,6 +61,17 @@ VertexFactory *
 new_vf ( int startsize, int ngenes, void ( *clear_mem ) ( void * ),
          void *arg )
 {
+    //startsize is set to VFWIDTH by default
+    //when ngenes is big, following malloc will alloc super big memory block
+    //reduce the startsize then
+    int fitsize = startsize;
+    while( fitsize*ngenes >= startsize*64 )
+    {
+        fitsize = (int)(fitsize/2);
+        fprintf(stdout,"ngenes is %d,reduce the startsize to %d\n",ngenes,fitsize);
+        fflush(stdout);
+    };
+    startsize = fitsize;
     int i;
     VertexFactory *vf;
     vf = ( VertexFactory * ) malloc ( sizeof ( VertexFactory ) );
@@ -72,16 +87,22 @@ new_vf ( int startsize, int ngenes, void ( *clear_mem ) ( void * ),
     vf->length = startsize;
     vf->clear_mem = clear_mem;
     vf->clear_mem_arg = arg;
+
     for ( i = 0; i < startsize - 1; i++ )
     {
         vf->vertices[0][i].perm = &( vf->perms[0][i * ngenes] );
         vf->vertices[0][i].next = &( vf->vertices[0][i + 1] );
     }
     vf->vertices[0][i].perm = &( vf->perms[0][i * ngenes] );
-    vf->vertices[0][i].next = NULL; /* to avoid the if block in the 
-                                       for block */
+    vf->vertices[0][i].next = vf->tail; 
+    //vf->vertices[0][i].next = NULL; 
+    /* to avoid the if block in the for block */
     vf->tail = &( vf->vertices[0][i] );
     vf->head = &( vf->vertices[0][0] );
+
+    vf->offseth = 0; 
+    vf->offsett = i;
+    vf->full = 0; // sign if we already init all memory in the block.
 
 #ifdef THREADSAFE
     pthread_mutex_init ( &vf->mutex, NULL );
@@ -100,11 +121,27 @@ get_vertex ( VertexFactory * vf )
 #ifdef THREADSAFE
     pthread_mutex_lock ( &vf->mutex );
 #endif
+//what if we reach the end of vf->vertices, reuse the first one of it?
+//check vf->width <>  VFWIDTH
 
-    if ( vf->head == NULL )
-    {                           /*vf->nalloc == vf->capacity) { */
+    if ( vf->width >= VFWIDTH )     
+    {
+        fprintf(stdout,"reach the end of VFWIDTH, reuse the memory\n");
+        fflush(stdout);
+        vf->full = 1;
+        vf->width = 1;
+        vf->nalloc=0;
+        vf->offseth = 0;
+        vf->offsett = VFSIZE-1;
+        vf->tail = &( vf->vertices[0][VFSIZE-1] );
+        vf->head = &( vf->vertices[0][0] );
+    }
+    
+    if (( vf->head == NULL ))/*|| (vf->offseth> (VFSIZE -1)) || (vf->offsett>(VFSIZE-1))*/
+    {
         length = vf->length;
         width1 = vf->width - 1;
+       // fprintf(stdout,"end of width1=%d\n",width1); fflush(stdout);
         if ( vf->clear_mem != NULL )
         {
 #ifdef THREADSAFE
@@ -120,12 +157,20 @@ get_vertex ( VertexFactory * vf )
         oldcap = vf->capacity;
         vf->width++;
         width1 = vf->width - 1;
+    //    fprintf(stdout,"vf->width++=%d\n",vf->width); fflush(stdout);
+        if(0==vf->full) //this is the first time we go through this memory block
+        {
         vf->vertices[width1] =
             ( Vertex * ) malloc ( length * sizeof ( Vertex ) );
         vf->perms[width1] =
             ( int * ) malloc ( length * vf->uncondensedNumGenes *
                                sizeof ( int ) );
         vf->capacity += length;
+        }
+        else { 
+        //we already went through the memory block,
+        //no need to alloc again
+        }
         for ( i = 0; i < length - 1; i++ )
         {
             vf->vertices[width1][i].perm =
@@ -136,13 +181,17 @@ get_vertex ( VertexFactory * vf )
         vf->vertices[width1][i].next = NULL;
         vf->tail = &( vf->vertices[width1][i] );
         vf->head = &( vf->vertices[width1][0] );
+        vf->offseth = 0;
+        vf->offsett = i;
     }
-
-
-    vf->nalloc++;
+    //fprintf(stdout,"get vertex at offsetH=%d\n",vf->offseth); fflush(stdout);
+    
+    vf->nalloc ++;
+    vf->offseth ++;
 
     retval = vf->head;
     vf->head = retval->next;
+
 
 #ifdef THREADSAFE
     pthread_mutex_unlock ( &vf->mutex );
@@ -154,19 +203,26 @@ get_vertex ( VertexFactory * vf )
 void
 return_vertex ( VertexFactory * vf, Vertex * v )
 {
+    /*
 #ifdef THREADSAFE
     pthread_mutex_lock ( &vf->mutex );
 #endif
 
-/*   printf("%d\n", v->memidx); */
     vf->nalloc--;
-    ( vf->tail )->next = v;
+    vf->offsett ++;
+
+    fprintf(stdout,"add vertex to offsetT=%d,offsetH is %d\n",
+            vf->offsett,vf->offseth);
+    fflush(stdout);
+   
+   ( vf->tail )->next = v;
     vf->tail = v;
     v->next = NULL;
 
 #ifdef THREADSAFE
     pthread_mutex_unlock ( &vf->mutex );
 #endif
+*/
 }
 
 void
@@ -174,7 +230,8 @@ vf_free ( VertexFactory * vf )
 {
     int i;
     int width;
-    width = vf->width;
+    if (1==vf->full) width = VFWIDTH;
+    else width = vf->width;
     for ( i = 0; i < width; i++ )
         free ( vf->vertices[i] );
     for ( i = 0; i < width; i++ )
