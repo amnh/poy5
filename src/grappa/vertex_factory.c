5f0d172c0f9e93523d2bbc0c8ba82588f1a188cd
lin hong <lhong@amnh.org>
2009-12-08 16:57:25 -0500

    add "inuse" sign to vertex, when the vertex is in candidate stack. also add function "mark" and "unmark" the inuse sign. Add "freeveertex" to vertex factory, keep record of how many free vertex we have now, when all of the vertex are "inuse", stop the algorithm.

--- src/grappa/vertex_factory.c
+++ src/grappa/vertex_factory.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 
 #define VFWIDTH 512 //1024 
-//VFWIDTH should be variable, decided by gene size
+//VFWIDTH should be variable by gene size
 
 void
 clean_vf ( VertexFactory * vf, int ngenes, void ( *clear_mem ) ( void * ),
@@ -20,7 +20,6 @@ clean_vf ( VertexFactory * vf, int ngenes, void ( *clear_mem ) ( void * ),
     int i, j;
     int length1;
     vf->offseth = 0;
-    vf->offsett = length1;
     vf->nalloc = 0;
     vf->ngenes = ngenes;
     vf->clear_mem = clear_mem;
@@ -54,6 +53,53 @@ clean_vf ( VertexFactory * vf, int ngenes, void ( *clear_mem ) ( void * ),
 
 }
 
+
+int
+vf_no_free_vertex ( VertexFactory * vf ,int ps_size)
+{
+ /*   int totalsize;
+    if( 1 == vf->full)
+        totalsize = VFWIDTH*(vf->length);
+    else
+         totalsize = vf->capacity;
+    if ((totalsize-1)>ps_size)
+    {
+    fprintf(stdout,"free vertex: %d-1 > %d\n",totalsize,ps_size);
+        return 0;    
+    }
+    else
+    {
+        return 1;
+    }
+    */
+    if ( vf->freevertex > 0) return 0;
+    else return 1;
+}
+
+
+void
+vf_clear ( VertexFactory * vf)
+{
+    int i,j;
+    int wide, len;
+    wide = vf-> length;
+    if(1==vf->full)
+    {
+        vf->freevertex = VFWIDTH*(vf->length);
+        len = VFWIDTH;
+    }
+    else
+    {
+        vf->freevertex = vf->capacity;
+        len = vf->width;
+    }
+    for ( i = 0; i < len; i++ )
+    {
+        for ( j = 0; j < wide; j++ )
+            vf->vertices[i][j].inuse = 0;
+    }
+}
+
 /* Last two arguments pertain to a function to be called when capacity
    has been reached, before reallocating more memory.  Pass NULL for
    both if you do not wish to specify such a function */
@@ -61,16 +107,15 @@ VertexFactory *
 new_vf ( int startsize, int ngenes, void ( *clear_mem ) ( void * ),
          void *arg )
 {
-    //startsize is set to VFWIDTH by default
-    //when ngenes is big, following malloc will alloc super big memory block
+    //startsize is set to VFWIDTH by default = 512
+    //when ngenes is too big, following malloc will alloc super big memory block
     //reduce the startsize then
     int fitsize = startsize;
-    while( fitsize*ngenes >= startsize*64 )
+    if(ngenes>64)
     {
-        fitsize = (int)(fitsize/2);
+        fitsize = (int)( fitsize/ ((int)(ngenes/64)+1) );
         fprintf(stdout,"ngenes is %d,reduce the startsize to %d\n",ngenes,fitsize);
-        fflush(stdout);
-    };
+    }
     startsize = fitsize;
     int i;
     VertexFactory *vf;
@@ -94,15 +139,15 @@ new_vf ( int startsize, int ngenes, void ( *clear_mem ) ( void * ),
         vf->vertices[0][i].next = &( vf->vertices[0][i + 1] );
     }
     vf->vertices[0][i].perm = &( vf->perms[0][i * ngenes] );
-    vf->vertices[0][i].next = vf->tail; 
-    //vf->vertices[0][i].next = NULL; 
+    //vf->vertices[0][i].next = vf->tail; 
+    vf->vertices[0][i].next = NULL; 
     /* to avoid the if block in the for block */
     vf->tail = &( vf->vertices[0][i] );
     vf->head = &( vf->vertices[0][0] );
 
     vf->offseth = 0; 
-    vf->offsett = i;
     vf->full = 0; // sign if we already init all memory in the block.
+    vf->freevertex = startsize;
 
 #ifdef THREADSAFE
     pthread_mutex_init ( &vf->mutex, NULL );
@@ -126,18 +171,16 @@ get_vertex ( VertexFactory * vf )
 
     if ( vf->width >= VFWIDTH )     
     {
-        fprintf(stdout,"reach the end of VFWIDTH, reuse the memory\n");
-        fflush(stdout);
+// fprintf(stdout,"reach the end of VFWIDTH, reuse memory\n"); fflush(stdout);
         vf->full = 1;
         vf->width = 1;
         vf->nalloc=0;
         vf->offseth = 0;
-        vf->offsett = VFSIZE-1;
-        vf->tail = &( vf->vertices[0][VFSIZE-1] );
+        vf->tail = &( vf->vertices[0][vf->length-1] );
         vf->head = &( vf->vertices[0][0] );
     }
     
-    if (( vf->head == NULL ))/*|| (vf->offseth> (VFSIZE -1)) || (vf->offsett>(VFSIZE-1))*/
+    if (( vf->head == NULL ))
     {
         length = vf->length;
         width1 = vf->width - 1;
@@ -153,12 +196,11 @@ get_vertex ( VertexFactory * vf )
             pthread_mutex_lock ( &vf->mutex );
 #endif
         }
-
         oldcap = vf->capacity;
         vf->width++;
         width1 = vf->width - 1;
     //    fprintf(stdout,"vf->width++=%d\n",vf->width); fflush(stdout);
-        if(0==vf->full) //this is the first time we go through this memory block
+        if( 0 == vf->full) //this is the first time we go through this memory block
         {
         vf->vertices[width1] =
             ( Vertex * ) malloc ( length * sizeof ( Vertex ) );
@@ -166,6 +208,7 @@ get_vertex ( VertexFactory * vf )
             ( int * ) malloc ( length * vf->uncondensedNumGenes *
                                sizeof ( int ) );
         vf->capacity += length;
+        vf->freevertex += length;
         }
         else { 
         //we already went through the memory block,
@@ -182,7 +225,6 @@ get_vertex ( VertexFactory * vf )
         vf->tail = &( vf->vertices[width1][i] );
         vf->head = &( vf->vertices[width1][0] );
         vf->offseth = 0;
-        vf->offsett = i;
     }
     //fprintf(stdout,"get vertex at offsetH=%d\n",vf->offseth); fflush(stdout);
     
@@ -190,8 +232,21 @@ get_vertex ( VertexFactory * vf )
     vf->offseth ++;
 
     retval = vf->head;
-    vf->head = retval->next;
+//    retval->NO = vf->nalloc;
+    if( 1==(retval->inuse))
+    {
+ //       fprintf(stdout,"current vertex is in use, get the next one,freevertex=%d \n", vf->freevertex);
+        vf->head = retval->next;
+        retval = get_vertex(vf);
+    }
+    else
+    {
+ //       retval->inuse = 1;
+        vf->head = retval->next;
+    //    vf->freevertex --;
+    }
 
+  //      vf->head = retval->next;
 
 #ifdef THREADSAFE
     pthread_mutex_unlock ( &vf->mutex );
@@ -200,9 +255,20 @@ get_vertex ( VertexFactory * vf )
     return retval;
 }
 
+
+void
+mark_vertex (  VertexFactory * vf, Vertex * v )
+{
+    vf->freevertex --;
+    v->inuse = 1;
+}
+
+
 void
 return_vertex ( VertexFactory * vf, Vertex * v )
 {
+    v->inuse = 0;
+  //  vf->freevertex ++;
     /*
 #ifdef THREADSAFE
     pthread_mutex_lock ( &vf->mutex );
