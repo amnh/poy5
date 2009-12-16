ee76618b5a22e54dfa9d7909d421a3438f497e59
lin hong <lhong@amnh.org>
2009-12-16 17:12:22 -0500

    change sign "CONCORDE" to "USE_CONCORDE" in lk_main.h

--- src/grappa/lk_main.h
+++ src/grappa/lk_main.h
@@ -1,7 +1,7 @@
 #ifndef LK_MAIN_H
 #define LK_MAIN_H
 
-#ifdef CONCORDE
+#ifdef USE_CONCORDE
 int chlinkern ( int ncount, int **adj, int *tour, int *incycle,
                 int *outcycle );
 int greedylk ( int ncount, int **adj, int *tour, int *incycle,
