f4e185a393681e0a0f6d5314dad1cb357788e868
lin hong <lhong@amnh.org>
2009-12-30 15:36:23 -0500

    see changes in lk_main.c

--- src/grappa/lk_main.h
+++ src/grappa/lk_main.h
@@ -2,6 +2,16 @@
 #define LK_MAIN_H
 
 #ifdef USE_CONCORDE
+
+#include "machdefs.h"
+#include "linkern.h"
+#include "util.h"
+#include "kdtree.h"
+#include "edgegen.h"
+#include "macrorus.h"
+#include "tsp.h"
+
+
 int chlinkern ( int ncount, int **adj, int *tour, int *incycle,
                 int *outcycle );
 int greedylk ( int ncount, int **adj, int *tour, int *incycle,
