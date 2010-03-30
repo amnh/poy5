08c1a5adc47f0b365a8f0ea762bdf2ef0ba1ea7e
Nicholas Lucaroni <nicholas.r.lucaroni@gmail.com>
2010-03-30 11:29:19 -0400

    Bugfix; reading trees with scientific notation on branch lengths
    Symptom  - Cannot read tree with floats in scientific notation, with capital E.
    Cause    - Parser did not include this format for uppercase E's.
    Solution - Add E to character class with e.
    
    Also added scientific notation support for outdated tree parser in Tree.

--- src/tree.ml
+++ src/tree.ml
@@ -1715,8 +1715,9 @@ module Parse = struct
                 in
                 Some (float_of_string str)
             | ('0' .. '9') as v -> read_branch_length (v :: acc)
+            | 'e' -> read_branch_length ('e' :: acc)
+            | 'E' -> read_branch_length ('E' :: acc)
             | '.' -> read_branch_length ('.' :: acc)
-            | ':' -> read_branch_length acc
             |  v   -> raise (Illegal_tree_format ("Unexpected Char "^(Char.escaped v)))
         in
 
