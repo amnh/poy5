8a4cba332bd4d2347f0ff4db1fb448275af87d4b
Nicholas Lucaroni <nicholas.r.lucaroni@gmail.com>
2010-05-06 13:21:09 -0400

    bugfix; adjusted the lk model program to use gtr instead of general files
    symptom - strange results with high branch lengths; approach 0, not priors.
    cause   - m_file is broken; todo
    solution- use the gtr model in calculating the matrix
    caveat  - the m_file function is still broken

--- src/lk.ml
+++ src/lk.ml
@@ -10,33 +10,22 @@ let main () =
                             with | _ -> ~-.1.0
         in
         (* process read_floatmatrix output *)
-        let priors,rate_matrix = match data with
-            | hd::tl ->
-                let pi = Array.of_list hd
-                and ms = Array.of_list (List.map Array.of_list tl) in
-                (pi,ms)
+        let priors,coefficients = match data with
+            | hd::co::[] ->
+                let pi = Array.of_list hd and co = Array.of_list co in
+                (pi,co)
             | _ -> failwith "Matrix file in an incorrect format."
         in
         (* create the matrix; and multiply through by priors *)
+        let alpha_size = Array.length priors in
         let subst_matrix = 
-            let alpha_size = Array.length priors in
-            let subst = try MlModel.m_file priors rate_matrix alpha_size
-                        with | _ -> failwith "Matrix size and Priors are inconsistent"
-            in
-            for i = 0 to alpha_size-1 do
-                for j = (i+1) to alpha_size - 1 do
-                    subst.{i,j} <- subst.{i,j} *. priors.(j);
-                    subst.{j,i} <- subst.{j,i} *. priors.(i);
-                done;
-            done;
-            MlModel.m_meanrate subst priors;
-            subst
+            try MlModel.m_gtr priors coefficients alpha_size
+            with | _ -> failwith "Matrix size and Priors are inconsistent"
         in
-        Printf.printf "Rate matrix multiplied priors w/ mean rate = 1\n";
-        MlStaticCS.print_barray2 subst_matrix;
-
         let final_matrix = MlModel.compose_model subst_matrix branch_length in
 
+        Printf.printf "Rate matrix multiplied priors w/ mean rate = 1\n";
+        MlStaticCS.print_barray2 subst_matrix;
         if branch_length < 0.0 then Printf.printf "Instantanious Rate Matrix:\n"
         else Printf.printf "Transition Probability Matrix for t = %f\n" branch_length;
         MlStaticCS.print_barray2 final_matrix
