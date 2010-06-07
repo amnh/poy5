dd413a1a19a9947ea4ad37d56723c11b2967b11d
Nicholas Lucaroni <nicholas.r.lucaroni@gmail.com>
2010-06-07 17:59:14 -0400

    bugfix; tree_compare bug fixes
    bugfix -
        symptom - had RF distance > 0 on differently rooted tree
        cause   - using intersection instead of set difference from all elements
        solution- use set difference
    
    bugfix -
        symptom - off by one error
        cause   - rooted trees had an extra partition
        solution- skip the root; otherwise normal algorithm

--- src/utils/tree_compare.ml
+++ src/utils/tree_compare.ml
@@ -1,13 +1,16 @@
 let failwithf format = Printf.ksprintf failwith format
 let (-->) b a = a b
 
-let print_set set = 
+let print_set all set = 
+    let opp = All_sets.Integers.diff all set in
     Printf.printf "(";
     All_sets.Integers.iter (fun e -> Printf.printf "%d " e) set;
+    Printf.printf ") / (";
+    All_sets.Integers.iter (fun e -> Printf.printf "%d " e) opp;
     Printf.printf ")"
 
 let read_tree filename = 
-    List.map (fun ts -> List.map (fun t -> (filename,t)) ts) 
+    List.map (fun ts -> List.map (fun t -> (Some filename,t)) ts) 
              (Tree.Parse.of_channel (open_in filename))
 
 let read_tree_nexus filename : (string option * Tree.Parse.tree_types) list =
@@ -25,18 +28,14 @@ let read_tree_nexus filename : (string option * Tree.Parse.tree_types) list =
             | _ -> failwith "nope")
         parsed
 
-let load_trees lst = 
-    lst --> List.map read_tree_nexus
-        --> List.flatten
-        --> List.map (fun (n,t) -> (n,Tree.Parse.cannonic_order t))
-
-(*    lst --> List.map read_tree*)
-(*        --> List.flatten --> List.flatten*)
-(*        --> List.map (fun (f,t) -> (f,Tree.Parse.cannonic_order t))*)
+let load_trees is_nexus lst = 
+    let res = if is_nexus then List.map read_tree_nexus lst
+              else List.flatten (List.map read_tree lst) in
+    List.map (fun (f,t) -> (f,Tree.Parse.cannonic_order t)) (List.flatten res)
  
 let create_partitions tbl tree =
     let rec union_lst acc = function
-        | []     -> acc
+        | []         -> acc
         | (hd,_)::tl -> union_lst (All_sets.Integers.union acc hd) tl
     and traversal f b = function
         | Tree.Parse.Leafp d -> 
@@ -44,20 +43,25 @@ let create_partitions tbl tree =
         | Tree.Parse.Nodep (childs,data) ->
             let subtree_sets = List.concat (List.map (traversal f b) childs) in
             (union_lst All_sets.Integers.empty subtree_sets,b data) :: (subtree_sets)
+    and skip_root_traversal f b = function
+        | Tree.Parse.Nodep (childs,data) ->
+            List.flatten (List.map (traversal f b) childs)
+        | Tree.Parse.Leafp d -> 
+            [(All_sets.Integers.singleton (Hashtbl.find tbl (f d)),b d)]
     in
     match tree with
     | Tree.Parse.Annotated (t,a) ->
-            traversal (fun x -> x) (fun _ -> None) t
+            skip_root_traversal (fun x -> x) (fun _ -> None) t
     | Tree.Parse.Branches t ->
-            traversal (fun x -> fst x) (fun (_,b) -> b) t
+            skip_root_traversal (fun x -> fst x) (fun (_,b) -> b) t
     | Tree.Parse.Flat t ->
-            traversal (fun x -> x) (fun _ -> None) t
+            skip_root_traversal (fun x -> x) (fun _ -> None) t
     | Tree.Parse.Characters t ->
-            traversal (fun x -> fst x) (fun _ -> None) t
+            skip_root_traversal (fun x -> fst x) (fun _ -> None) t
  
-(* they must be equal, or intersection = null *)
+(* they must be equal *)
 let compare_partitions all a b =
-    let other_b = All_sets.Integers.inter all b in
+    let other_b = All_sets.Integers.diff all b in
     let equality_one = All_sets.Integers.compare a b
     and equality_two = All_sets.Integers.compare a other_b in
     (equality_one = 0) || (equality_two = 0)
@@ -73,11 +77,11 @@ let compare_branches table set_of_all partitions_one partitions_two =
             let b_len = match b with | Some x -> x | None -> 0.0 
             and y_len = match y with | Some x -> x | None -> 0.0 in
             if (abs_float (b_len -. y_len)) >= 1.0e-2 then begin
-                    Printf.printf "\t%s\t%s  (%f)\t" 
-                        (match b with | Some x -> string_of_float x | None -> "none\t")
-                        (match y with | Some x -> string_of_float x | None -> "none\t") 
+                    Printf.printf "\t%2.5f\t\t%2.5f\t\t(%2.5f)\t" 
+                        (match b with | Some x -> x | None -> 0.0)
+                        (match y with | Some x -> x | None -> 0.0) 
                         (abs_float (b_len -. y_len));
-                    print_set set;
+                    print_set set_of_all set;
                     print_newline ();
                     incr modified
             end)
@@ -85,34 +89,27 @@ let compare_branches table set_of_all partitions_one partitions_two =
     if !modified = 0 then Printf.printf "\tSame branch lengths\n%!"
 
 let compare_trees verbose taxon_tbl set_of_all t1 t2 = 
+    let comparer elm (x,_) = compare_partitions set_of_all elm x in
     let partitions_one = create_partitions taxon_tbl t1
     and partitions_two = create_partitions taxon_tbl t2 in
-    let s,a = 
+
+    let s = 
         List.fold_left
-            (fun (same,all) (elm,_) ->
-                try let _ = 
-                        List.find (fun (x,_) -> compare_partitions set_of_all elm x)
-                                  partitions_two
-                    in 
-                    (same+1,all+1)
-                with | Not_found ->
-                    if verbose then begin
-                        Printf.printf "\t"; print_set elm; print_newline ()
-                    end;
-                    (same,all+1))
-            (0, 0)
+            (fun diff (elm,_) ->
+                if List.exists (comparer elm) partitions_two then
+                    diff
+                else if verbose then begin
+                    Printf.printf "\t"; print_set set_of_all elm; print_newline ();
+                    diff+1
+                    end
+                else
+                    diff+1)
+            0
             partitions_one
     in
-
-    if verbose then begin
-        if s = a then begin
-            Printf.printf "\tSame topology\n%!";
-            compare_branches taxon_tbl set_of_all partitions_one partitions_two
-        end else
-            Printf.printf "\tOf %d partitions, %d are the same\n%!" a s
-    end;
-    a - s
-
+    if s = 0 && verbose then
+        compare_branches taxon_tbl set_of_all partitions_one partitions_two;
+    s
 
 let create_taxon_table tree = 
     let res = Hashtbl.create 31
@@ -166,20 +163,41 @@ let compare_matrix verbose all =
     let tree_assoc = Array.of_list all in
     let length = Array.length tree_assoc in
     let costs_matrix = Array.make_matrix length length 0 in
+    (* fill matrix with distance from ROW->COLUMN *)
+    let () = 
+        matrix_destructive_map false costs_matrix
+            (fun i j -> 
+                if i = j then 0 
+                else compare_trees verbose table set_of_all
+                        (snd tree_assoc.(i)) (snd tree_assoc.(j)))
+    in
+    (* we have distance in one direction; A->B and B->A *)
     let () = 
         matrix_destructive_map true costs_matrix
-            (fun i j -> 
-                compare_trees verbose table set_of_all
-                    (snd tree_assoc.(i)) (snd tree_assoc.(j)))
+            (fun i j -> costs_matrix.(i).(j) + costs_matrix.(j).(i))
     in
     print_matrix tree_assoc costs_matrix;
     ()
 
+let verbose_single hd =
+    let print_partitions set_of_all partitions = 
+        List.iter (fun (s,t) -> print_set set_of_all s;print_newline ()) partitions;
+        print_newline ()
+    in
+    let table = create_taxon_table (snd hd) in
+    let set_of_all = 
+        Hashtbl.fold (fun name code set -> All_sets.Integers.add code set)
+                     table (All_sets.Integers.empty)
+    in
+    let partitions = create_partitions table (snd hd) in
+    print_table table; 
+    print_partitions set_of_all partitions
+
 let () = match Array.to_list Sys.argv with
     | []     -> failwith  "no arguments?"
     | hd::[] -> failwithf "usage: %s <treefile1> <treefile2> ..." hd
-    | _::tl ->
-        let verbose,tl = 
+    | _::tl  ->
+        let v, tl = 
             if (List.mem "-v" tl) || ( List.mem "--verbose" tl) then
                 let tl_minus = 
                     List.filter (fun x -> match x with 
@@ -190,10 +208,9 @@ let () = match Array.to_list Sys.argv with
             else 
                 false,tl
         in
-        begin match load_trees tl with
-            | []        -> failwith "No trees loaded; require at least two"
-            | hd::[]    -> failwith "One tree loaded; require at least two"
-            | (hd::rest as all) -> compare_matrix verbose all
+        begin match load_trees true tl with
+            | []             -> failwith "No trees loaded; require at least two."
+            | hd::[] when v  -> verbose_single hd
+            | hd::[]         -> failwith "One tree loaded; require at least two."
+            | (hd::_ as all) -> compare_matrix v all
         end
-
-
