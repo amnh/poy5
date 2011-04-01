f098459013c52b8b0765bebc2aded75e2c0ae531
lin hong <lhong@amnh.org>
2011-04-01 12:24:35 -0400

    add comment to binaryTree.ml and binaryTree.mli

--- src/binaryTree.ml
+++ src/binaryTree.ml
@@ -1,3 +1,23 @@
+(* POY 4.0 Beta. A phylogenetic analysis program using Dynamic Homologies.    *)
+(* Copyright (C) 2007  Andrés Varón, Le Sy Vinh, Illya Bomash, Ward Wheeler,  *)
+(* and the American Museum of Natural History.                                *)
+(*                                                                            *)
+(* This program is free software; you can redistribute it and/or modify       *)
+(* it under the terms of the GNU General Public License as published by       *)
+(* the Free Software Foundation; either version 2 of the License, or          *)
+(* (at your option) any later version.                                        *)
+(*                                                                            *)
+(* This program is distributed in the hope that it will be useful,            *)
+(* but WITHOUT ANY WARRANTY; without even the implied warranty of             *)
+(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *)
+(* GNU General Public License for more details.                               *)
+(*                                                                            *)
+(* You should have received a copy of the GNU General Public License          *)
+(* along with this program; if not, write to the Free Software                *)
+(* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301   *)
+(* USA                                                                        *)
+
+let () = SadmanOutput.register "BinaryTree" "$Revision: 1751 $"
 
 
 type ('k,'a) b_tree =
@@ -5,7 +25,6 @@ type ('k,'a) b_tree =
     | Leaf of 'k * 'a 
     | Node of 'k * ('k,'a) b_tree * ('k,'a) b_tree
 
-(***************** binary tree function begins ****************************)
 
 let create_btree leafkey leafdata =  Leaf (leafkey,leafdata)
 
@@ -87,7 +106,7 @@ let search_in_btree key bt printkey_f printnode_f =
     in
     search_node bt key
 
-let remove_from_btree seedseq bt printkey_f printnode_f = (*key in b_tree is seedseq*)
+let remove_from_btree seedseq bt printkey_f printnode_f = 
     let rec remove_node sub_bt key = 
         match sub_bt with 
         | Node (middle_key, left_bt, right_bt) ->
