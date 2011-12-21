de7b463b31621f0d9aa710956128d44d5e75e5e2
Nicholas Lucaroni <nicholas.r.lucaroni@gmail.com>
2011-12-21 18:45:57 -0500

    Added copyright information

--- src/likelihood.h
+++ src/likelihood.h
@@ -16,6 +16,7 @@
 /* along with this program; if not, write to the Free Software                */
 /* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301   */
 /* USA                                                                        */
+
 #include "config.h"         //defines, if likelihood, USE_LIKELIHOOD
 #ifdef USE_LIKELIHOOD
 #include <stdio.h>
