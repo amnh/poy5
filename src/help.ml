f316bdaf49090a3f6ccd902e42cdf3c9ac75dea9
Nicholas Lucaroni <nicholas.r.lucaroni@gmail.com>
2013-06-26 18:14:47 -0400

    to over-ride help.ml generation from latex

---
+++ src/help.ml
@@ -0,0 +1,69 @@
+let authors = ""
+let copyright = ""
+let version = ""
+let build = ""
+let calculate_support = ""
+let clear_memory = ""
+let cd = ""
+let echo = ""
+let exit = ""
+let fuse = ""
+let help = ""
+let inspect = ""
+let load = ""
+let perturb = ""
+let pwd = ""
+let quit = ""
+let read = ""
+let recover = ""
+let rediagnose = ""
+let redraw = ""
+let rename = ""
+let report = ""
+let run = ""
+let save = ""
+let search = ""
+let select = ""
+let set = ""
+let store = ""
+let swap = ""
+let transform = ""
+let use = ""
+let version = ""
+let wipe = ""
+
+let index = [
+    ("authors", authors);
+    ("copyright", copyright);
+    ("version", version);
+    ("build", build);
+    ("calculate_support", calculate_support);
+    ("clear_memory", clear_memory);
+    ("cd", cd);
+    ("echo", echo);
+    ("exit", exit);
+    ("fuse", fuse);
+    ("help", help);
+    ("inspect", inspect);
+    ("load", load);
+    ("perturb", perturb);
+    ("pwd", pwd);
+    ("quit", quit);
+    ("read", read);
+    ("recover", recover);
+    ("rediagnose", rediagnose);
+    ("redraw", redraw);
+    ("rename", rename);
+    ("report", report);
+    ("run", run);
+    ("save", save);
+    ("search", search);
+    ("select", select);
+    ("set", set);
+    ("store", store);
+    ("swap", swap);
+    ("transform", transform);
+    ("use", use);
+    ("version", version);
+    ("wipe", wipe);
+]
